19f39de79aaa84ba9a17212bf7a5213994a5a49e
José Carlos Mar Rangel <68357760+JoseCarlosMarRangel@users.noreply.github.com>
2022-10-01 14:23:23 -0500

    basedeproyecto

---
+++ EjemploTriggers/CambiarColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace EjemploTriggers
+{
+    // ! Uso de clase abstracta triggeraction Button
+    public class CambiarColor :TriggerAction<Button>
+    {
+        // ! Metodo que cambia el color y cambia el valor de hola mundo
+        protected override void Invoke(Button sender)
+        {
+            sender.BackgroundColor = Color.DarkOrange;
+            sender.Text = "Hola hola mundo";
+        }
+    }
+}
---
+++ EjemploTriggers/Nombre.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EjemploTriggers
+{
+    public class Nombre : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value is string && ((string)value).Length > 4)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
