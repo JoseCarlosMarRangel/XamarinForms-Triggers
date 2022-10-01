5f906365f963dbc3cfdffaa0a67be9853f7d35d5
José Carlos Mar Rangel <68357760+JoseCarlosMarRangel@users.noreply.github.com>
2022-10-01 01:29:50 -0500

    Ejemplo triggers
    Ejemplo de triggers

---
+++ EjemploTriggers/CambiarColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms;
+
+namespace EjemploTriggers
+{
+    public class CambiarColor :TriggerAction<Button>
+    {
+        // ! Metodo que cambia el color y cambia el valor de hola mundo
+        protected override void Invoke(Button sender)
+        {
+            sender.BackgroundColor = Color.DarkOrange;
+            sender.Text = "Hola hola mundo";
+        }
+    }
+}
